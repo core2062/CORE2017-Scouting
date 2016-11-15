5e1f9f9b9f8062ab9bea903324d69c33b38ba80c
Draven Schilling <Dravenschilling@gmail.com>
2016-11-14 19:34:55 -0600

    Added some documentation
    Setup for match report

--- Constants.py
+++ Constants.py
@@ -1,3 +1,4 @@
+""" Input form values """
 allNames = []
 
 checkbox_names = [
@@ -15,6 +16,11 @@ text_names = [
     'comments'
 ]
 
+radio_values = {
+    'defenceType': ('Moat', 'Rockwall', 'RoughTerrain'),
+    'hasDefender': ('Yes', 'No')
+}
+
 for checkbox in checkbox_names:
     allNames.append(checkbox)
 for radio in radio_names:
@@ -24,7 +30,14 @@ for number in number_names:
 for text in text_names:
     allNames.append(text)
 
-radio_values = {
-    'defenceType': ('Moat', 'Rockwall', 'RoughTerrain'),
-    'hasDefender': ('Yes', 'No')
-}
+""" Match Report Values """
+
+team_number_fields = [
+    'RedTeam1',
+    'RedTeam2',
+    'RedTeam3',
+    'BlueTeam1',
+    'BlueTeam2',
+    'BlueTeam3'
+]
+
--- Team.py
+++ Team.py
@@ -20,6 +20,7 @@ class Team(teamNumber):
             else:
                 print(err)
 
+        self.radio_values = Constants.radio_values
         self.cursor = self.dbConnection.cursor()
         self.teamNumber = teamNumber
         self.dataList = Constants.allNames
@@ -31,6 +32,9 @@ class Team(teamNumber):
             self.categoryDictionary[data] = self.allData[count]
 
     def verify_category(self, category):
+
+        """ Verifies the category exists
+            Otherwise  ____ """
         verified = 0
         for item in self.dataList:
             if item == category:
@@ -38,19 +42,32 @@ class Team(teamNumber):
         return verified
 
     def get_data(self, category):
+
+        """Returns category data in a tuple """
+
         if self.verify_category(category) == 1:
             for key in self.categoryDictionary:
                 if str(key) == str(category):
                     return self.categoryDictionary[key]
 
-    def sum_data(self, category):
+    def sum_data(self, category, category2='none'):
+
+        """ Number specific function which given one or two
+            categories adds all the values together """
+
         if self.verify_category(category) == 1:
             sum = 0
             for data in self.get_data(category):
                 sum += data
+            if category2 != 'none':
+                for itr in self.get_data(category2):
+                    sum += itr
             return sum
 
     def num_data_entries(self, category):
+
+        """ Returns number of times there is data in a category """
+
         if self.verify_category(category) == 1:
             count = 0
             for data in self.get_data(category):
@@ -58,10 +75,16 @@ class Team(teamNumber):
             return count
 
     def avg_data(self, category):
+
+        """ Finds the mean of a category number set """
+
         if self.verify_category(category) == 1:
             return self.sum_data(category)/self.num_data_entries(category)
 
     def max_in_data(self, category):
+
+        """ Finds the max in the category number set """
+
         if self.verify_category(category) == 1:
             count = 0
             for data in self.get_data(category):
@@ -73,6 +96,9 @@ class Team(teamNumber):
             return max
 
     def min_in_data(self, category):
+
+        """ Finds the min in the category number set """
+
         if self.verify_category(category) == 1:
             count = 0
             for data in self.get_data(category):
@@ -82,3 +108,17 @@ class Team(teamNumber):
                 if data < min:
                     min = data
             return min
+
+    def return_best(self, category, rankOrder):
+
+        """ Radio specific function which displays best option
+            rankOrder = Tuple of all radio options available in that category
+            ranked from worst to best """
+
+        if self.verify_category(category) == 1:
+            highest = 'N/A'
+            for item in rankOrder:
+                for data in self.get_data(category):
+                    if item == data:
+                        highest = data
+            return highest
